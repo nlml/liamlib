1f91d2e9a7704ecc5cf43fac4fd452008b6d720a
liam schoneveld <liam.sch@gmail.com>
2019-04-13 22:04:25 +0200

    upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='liamlib',
+      version='0.1',
+      description='general utils used by liam',
+      url='',
+      author='Liam Schoneveld',
+      author_email='',
+      license='',
+      packages=['liamlib'],
+      zip_safe=False)
+
